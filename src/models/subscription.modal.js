6e434aa2bf07c3197eca909ffb1ee2bf47e7ffe5
Mayank Kumar <mayankkumar@Mayanks-MacBook-Air.local>
2024-08-19 23:54:40 +0530

    subscription modal

--- src/models/subscription.modal.js
+++ src/models/subscription.modal.js
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose"
 
-const subscriptionSchema = new Schema({})
+const subscriptionSchema = new Schema({
+
+},{
+    timestamps: true,
+});
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
