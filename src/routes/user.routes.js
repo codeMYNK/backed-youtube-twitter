8a50b77f32db0d80958469390a865e13cc3e814a
Mayank Kumar <mayankkumar@Mayanks-MacBook-Air.local>
2024-08-21 17:02:59 +0530

    comment, dashboard, like, playlist controller added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,7 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserCHannelProfile,
+  getUserChannelProfile,
   getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -45,7 +45,7 @@ router
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
-router.route("/c/:username").get(verifyJWT, getUserCHannelProfile);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
